0e584a02f19470bed1aa005f1fd71ebec42362e1
Manuel Schierenberg <mail@elanum.de>
2021-05-24 21:26:40 +0200

    refactor: :art: update ping command
    updated the response of the ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,17 +3,18 @@ import Command from '../libs/command';
 
 class Ping extends Command {
   public constructor() {
-    super(
-      {
-        name: 'ping',
-        description: 'Replies with the current latency',
-      },
-    );
+    super({
+      name: 'ping',
+      description: 'Replies with the current latency',
+    });
   }
 
   public async sendResponse(interaction: CommandInteraction): Promise<void> {
     const ping = `Ping is ${Date.now() - interaction.createdTimestamp}ms`;
-    const response = new MessageEmbed().setTitle('Ping').setDescription(ping);
+    const response = new MessageEmbed()
+      .setColor('GREEN')
+      .setTitle('Ping')
+      .setDescription(ping);
     await interaction.reply({
       ephemeral: true,
       embeds: [response],
